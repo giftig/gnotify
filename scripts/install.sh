9d32b233878dee3cf1bc236d92cefe725e362d23
Rob Moore <giftiger.wunsch@xantoria.com>
2014-08-21 22:46:34 +0100

    Fix scripts/install.sh
    Didn't test it yet
    
    I was using 'read' wrong: fix that
    Make the script executable
    Test that it produces a fully functional installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-install_prefix='/usr/local/'
+install_prefix='/usr/local'
 
 if [[ "$1" != "" ]]; then
-  install_prefix="$1"
+  install_prefix="$(realpath $1)"
 fi
 
 echo "This script will install binaries, config, etc. to the system."
 echo "It'll use the install prefix '$install_prefix'."
 echo "Make sure you're running this as a user with write permissions there."
-read "OK? " resp
+echo -n 'OK? '
+read -n 1 resp
+echo ''
 
-if [[ "$(grep -E '^y')" != "" ]]; then
+if [[ "$resp" != "y" ]]; then
   echo 'Aborting.'
   exit 1
 fi
@@ -24,4 +26,4 @@ mkdir -p "$install_prefix/share"
 # Copy binary, conf, and shared files
 cp -f 'bin/xantoria.com' "$install_prefix/bin/gnotify"
 cp -f 'etc/gnotify.conf' "$install_prefix/etc/gnotify.conf"
-cp -R 'share/*' "$install_prefix/share"
+cp -R 'share' "$install_prefix"
