bbcd871dc72247ea13e27a3a0795106d71d6a889
Rob Moore <giftiger.wunsch@xantoria.com>
2015-07-13 22:54:56 +0100

    Improve config discovery
    Try the following sequence:
      * Config file provided as first argument to the application
      * ~/.gnotify/gnotify.conf
      * ../etc/gnotify.conf
      * Fatal error if none of the above are discovered
    
    Also log the settings file used

--- src/xantoria.com/gnotify/main.go
+++ src/xantoria.com/gnotify/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"os/user"
 	"path/filepath"
 
 	"xantoria.com/gnotify/config"
@@ -12,16 +13,47 @@ import (
 	"xantoria.com/gnotify/sources/todo"
 )
 
-func main() {
-	settingsFile, _ := filepath.Abs(filepath.Dir(os.Args[0]) + "/../etc/gnotify.conf")
+func pickSettingsFile() string {
+	// Did we provide an explicit config file? That takes precedence
 	if len(os.Args) > 1 {
-		settingsFile = os.Args[1]
+		f := os.Args[1]
+
+		if _, err := os.Stat(f); err != nil {
+			// If the specific config file provided is no good, fail immediately
+			log.Fatalf("Error in provided config file %s! %v", f, err)
+		}
+
+		return f
 	}
+
+	// Let's see if we the user has their own config file
+	usr, err := user.Current()
+	if err == nil {
+		f := usr.HomeDir + "/.gnotify/gnotify.conf"
+		if _, err := os.Stat(f); err == nil {
+			return f
+		}
+	}
+
+	// No user-specific config; let's try ../etc (eg. /opt/etc if we're installed to /opt/bin)
+	if f, err := filepath.Abs(filepath.Dir(os.Args[0]) + "/../etc/gnotify.conf"); err == nil {
+		if _, err := os.Stat(f); err == nil {
+			return f
+		}
+	}
+
+	log.Fatal("Couldn't find a usable config file!")
+	return ""
+}
+
+func main() {
+	settingsFile := pickSettingsFile()
 
 	// Load config and initialise log
 	config.LoadConfig(settingsFile)
 	log.Init()
-	log.Notice("Service starting...")
+	log.Notice("Service starting")
+	log.Info("Using settings %s", settingsFile)
 
 	notificationC := make(chan *notifier.Notification)
 
